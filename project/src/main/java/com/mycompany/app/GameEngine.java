1aeead829bd54983d0921d561bfa00ac87a3eacc
Toni <toni.heinonen@hotmail.fi>
2019-11-07 17:58:10 +0200

    Add test code.

--- project/src/main/java/com/mycompany/app/GameEngine.java
+++ project/src/main/java/com/mycompany/app/GameEngine.java
@@ -1,13 +1,66 @@
 package com.mycompany.app;
 
-/**
- * Hello world!
- *
- */
-public class GameEngine 
+public class GameEngine implements Runnable
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    private Thread thread;
+    public boolean running = false;
+    private final double UPDATE_CAP = 1.0/60.0;
+
+    public GameEngine() {
+
+    }
+
+    public void start() {
+        thread = new Thread(this);
+        thread.run();
+    }
+
+    public void stop() {
+
+    }
+
+    public void run() {
+        running = true;
+
+        boolean render = false;
+        double firstTime = 0;
+        double lastTime = System.nanoTime() / 1000000000.0;
+        double passedTime = 0;
+        double unprocessedTime = 0;
+
+        while(running) {
+            firstTime = System.nanoTime() / 1000000000.0;
+            passedTime = firstTime - lastTime;
+            lastTime = firstTime;
+
+            unprocessedTime += passedTime;
+
+            while(unprocessedTime >= UPDATE_CAP) {
+                unprocessedTime = UPDATE_CAP;
+                render = true;
+                System.out.println("update");
+                //TODO: Update game
+            }
+        }
+        
+        if(render) {
+            //TODO: REnder game
+        } else {
+            try {
+            Thread.sleep(1);
+            } catch(InterruptedException e) {
+            e.printStackTrace();
+            }
+        }
+        dispose();
+    }
+
+    public void dispose() {
+
+    }
+
+    public static void main(String[] args) {
+        GameEngine gc = new GameEngine();
+        gc.start();
     }
 }
