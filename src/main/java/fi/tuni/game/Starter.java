86dfb2a9b865598cc2ec3703b12c8d0def00c93d
Toni <toni.heinonen@hotmail.fi>
2019-11-19 20:32:44 +0200

    Make working jar package finally...

---
+++ src/main/java/fi/tuni/game/Starter.java
@@ -0,0 +1,12 @@
+package fi.tuni.game;
+
+/*
+    This seems to be the only way to create a working jar when using Java 11 and JavaFX.
+    I have tried at least 10 different ways, and this is the only one that worked.
+*/
+
+public class Starter {
+    public static void main(String[] args) {
+        Game.main(args);
+    }
+}
